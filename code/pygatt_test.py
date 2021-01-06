dc83d472ae48251596063bd0e286433cf71b1ff2
luhuadong <luhuadong@163.com>
2021-01-06 13:50:27 +0800

    add pygatt_test.py demo

---
+++ code/pygatt_test.py
@@ -0,0 +1,28 @@
+import time
+import pygatt
+
+adapter = pygatt.backends.GATTToolBackend()
+counter = 30
+threshold = 25
+
+try:
+    adapter.start()
+    device = adapter.connect('60:A4:23:C9:69:9C')
+
+    while counter > 0:
+        value = device.char_read("00002a6e-0000-1000-8000-00805f9b34fb")
+        temp = (value[1] * 256 + value[0]) / 100
+        print("[{:0>2d}] temp: {:.2f} 'C".format(counter, temp))
+
+        if temp > threshold:
+            device.char_write_handle(0x0022, bytearray([0x01]))  # LED on
+        else:
+            device.char_write_handle(0x0022, bytearray([0x00]))  # LED off
+
+        time.sleep(1)
+        counter -= 1
+
+finally:
+    print("end")
+    adapter.stop()
+
